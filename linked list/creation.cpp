98ccaa613bc9f5a499ae587a5536a07d4e1e1815
nikhil1-ux <nikhilyadav9044@gmail.com>
2026-09-10 01:47:28 +0530

    add implementation for linked list creation and traversal

---
+++ linked list/creation.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+
+  public:
+  int data;
+  Node* next;
+
+  Node(int value){
+    data = value;
+    next = NULL;
+  }
+};
+
+int main(){
+  int n;
+  cout<< " enter array size";
+  cin>> n;
+  int arr[n];
+
+  for(int i = 0; i < n; i++){
+   cin>> arr[i];
+  }
+
+  Node* head;
+  head == NULL;
+  if(head == NULL){
+    head = new Node(arr[0]);
+  }
+  for(int i = 1; i < n; i++ ){
+   
+    Node* temp;
+    temp = new Node(arr[i]);
+    head -> next = temp;
+    temp = head;
+  }
+
+  Node *newnode = head;
+  
+  while(newnode != NULL){
+  cout<< newnode -> data<< endl;
+  newnode = newnode ->next; 
+  }
+
+
+
+  return 0;
+
+}
